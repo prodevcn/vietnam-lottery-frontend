b916dbf96cba454cb972322072c58f9995057d1f
block-hac <cn.corestar@gmail.com>
2021-08-23 10:51:06 +0100

    ssl

--- app/constants/config.js
+++ app/constants/config.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 /** for local server */
-// export const API_URL = 'http://192.168.1.110:4332';
+export const API_URL = 'http://192.168.1.110:4332/api';
 
 /** for live server */
 
-export const API_URL = 'http://178.128.26.174:4332';
+// export const API_URL = 'http://178.128.26.174:4332';
+
+/** for ssl config */
+
+// export const API_URL = 'https://lotopoka.com/api';
