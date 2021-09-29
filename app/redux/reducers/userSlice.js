27dfd334767ce23528c7c3940f1c99c2e3a97ec2
block-hac <cn.corestar@gmail.com>
2021-09-29 17:48:25 +0800

    up

--- app/redux/reducers/userSlice.js
+++ app/redux/reducers/userSlice.js
@@ -2,7 +2,7 @@ import { SET_USER, REMOVE_USER, SET_ONLY_USER, SET_BALANCE } from "../../constan
 
 const INITIAL_STATE = {
   token: null,
-  balance: null,
+  balance: 0,
   user: null,
 };
 
@@ -26,6 +26,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
         user: null,
       };
     case SET_BALANCE:
+      console.log(action.payload);
       return {
         ...state,
         balance: action.payload
